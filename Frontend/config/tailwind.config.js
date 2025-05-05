dfbfd8c84f4278efa33bbb61a41e1e945cb4e800
ahmedali109 <ahmedalinaguib33@gmail.com>
2025-05-06 02:13:27 +0300

    - Add serverdown image - Create 403 & 404 html pages - Create 403 & 404 styles

---
+++ Frontend/config/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Lato", "sans-serif"],
+      },
+      animation: {
+        dip: "dip 1s",
+      },
+    },
+  },
+};
