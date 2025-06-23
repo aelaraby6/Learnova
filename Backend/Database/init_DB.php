8e2c53c39a7b43acfac0c66fff2a1b14a0340216
mohamedabdullah999 <mohaammed2562005@gmail.com>
2025-06-23 22:08:37 +0300

    setup project structure

--- Backend/Database/init_DB.php
+++
@@ -1,4 +0,0 @@
-<?php
-require "Database.php";
-$config = require ("config.php");
-$dbh = new Database($config['database']);
--- Backend/api/header.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE, PATCH");
