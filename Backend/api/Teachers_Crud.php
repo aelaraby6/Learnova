6c468ea36e75f663fb106815850bd6c873fcfb6b
Badr Mohamed <165401496+TrueBadr@users.noreply.github.com>
2025-07-18 11:12:30 +0300

    Update Teachers_Crud.php

--- Backend/api/Teachers_Crud.php
+++ Backend/api/Teachers_Crud.php
@@ -2,28 +2,27 @@
 
 require_once('../Database/Database.php');
 
-$input = json_decode(file_get_contents("php://input"), true); // استلام الطلب
+$input = json_decode(file_get_contents("php://input"), true);
 
-$data = new Database() ; // الاتصال مع الداتا بيز
-$connection = $data->connect();  // الارتباط مع الداتا بيز و كل الداتا ظاهرة دلوقتي
+$data = new Database() ; 
+$connection = $data->connect();  
 
-$method = $_SERVER['REQUEST_METHOD']; // معرفة نوع الطلب
+$method = $_SERVER['REQUEST_METHOD']; 
 
 
-if ($method==='GET') // عرض كل المدرسين
+if ($method==='GET')
 {
     $query = " select t1.teacher_id,t1.bio,t1.profile_picture,t1.facebook_link,t1.linkedin_link,t1.twitter_link,t2.user_id,t2.name,t2.email,t2.created_at from teacher t1
-              JOIN user t2 ON t1.user_id = t2.user_id";  // used join to retrive - teacher's name and email
+              JOIN user t2 ON t1.user_id = t2.user_id";  
 
     $order = $connection->prepare($query);
     $order->execute();
     $teachers = $order->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($teachers);
-    // تجهيز و تنفيذ الامر  ووضعه في اراي وارجاعه ك Json
 }
-else if ($method==='POST') // اضافة مدرس جديد
+else if ($method==='POST') 
 {
-    if (!isset($input['user_id'])) // لو اليوزر مش موجود اصلا
+    if (!isset($input['user_id']))
     {
         http_response_code(400);
         echo json_encode(['error' => 'User ID is not found']);
@@ -46,12 +45,11 @@ else if ($method==='POST') // اضافة مدرس جديد
 
         if ($Flag) echo json_encode(['message' => 'Teacher added Successfully']);
         else echo json_encode(['error' => 'Failed to add teacher']);
-        // تجهيز و تنفيذ الامر  وارجاعه رسالة ك Json
     }
 }
-else if ($method==='PUT') // تعديل مدرس موجود
+else if ($method==='PUT') 
 {
-    if (!isset($input['teacher_id'])) // لو اليوزر مش موجود اصلا
+    if (!isset($input['teacher_id'])) 
     {
         http_response_code(400);
         echo json_encode(['error' => 'Teacher ID is not found']);
@@ -71,12 +69,11 @@ else if ($method==='PUT') // تعديل مدرس موجود
         ]);
         if ($Flag) echo json_encode(['message' => ' the data of teacher is updated']);
         else echo json_encode(['error' => 'Failed to udpate data of the teacher']);
-        // تجهيز و تنفيذ الامر  وارجاعه رسالة ك Json
     }
 }
-else if ($method === 'DELETE') // لحذف مدرس موجود
+else if ($method === 'DELETE') 
 {
-    if (!isset($input['teacher_id'])) // لو اليوزر مش موجود اصلا
+    if (!isset($input['teacher_id'])) 
     {
         http_response_code(400);
         echo json_encode(['error' => ' Teacher ID is not found']);
@@ -87,7 +84,6 @@ else if ($method === 'DELETE') // لحذف مدرس موجود
         $flag = $order->execute([':teacher_id' => $input['teacher_id']]);
         if ($flag) echo json_encode(['message' => 'Teacher deleted']);
         else echo json_encode(['error' => 'Failed to delete teacher']);
-        // تجهيز و تنفيذ الامر  ووضعه في اراي وارجاعه رسالة ك Json
     }
 }
 ?>
