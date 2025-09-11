451c2d275a2d7eef7ea0a2540ba73d79fa9831fa
aelaraby6 <body1262005@gmail.com>
2025-09-11 23:43:56 +0300

    Add Signup & Login Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
+import Login from './Pages/Auth/Login/Login'
 
-import Header from './components/Header'
-import Footer from './components/Footer'
+// import Header from './components/Header'
+// import Footer from './components/Footer'
+import Signup from './Pages/Auth/SignUp/Signup'
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Footer />
+      {/* <Header />
+      <Footer /> */}
+      {/* <Signup/> */}
+      <Login/>
     </>
   )
 }
