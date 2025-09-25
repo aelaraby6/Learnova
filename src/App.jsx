e4f544cd24a2661447fa901459daca207c214448
aelaraby6 <body1262005@gmail.com>
2025-09-25 20:53:22 +0300

    Add Welcome Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import './App.css'
-import ProfilePage from './Pages/Profile/ProfilePage'
+import "./App.css";
+import Welcome from "./components/home/Welcome";
+// import ProfilePage from './Pages/Profile/ProfilePage'
 // import { CartProvider } from './context/CartContext'
 // import Cart from './components/Cart'
 // import Error404 from './components/Error404'
 // import Login from './Pages/Auth/Login/Login'
-// import Header from './components/Header'
+import Header from "./components/Header";
 // import Footer from './components/Footer'
 // import Signup from './Pages/Auth/SignUp/Signup'
 // import Error403 from './components/Error403'
 
 function App() {
-
   return (
-  //  <CartProvider>
-  //     {/* <Header />
-  //    <Footer /> 
-  //     {/* <Signup/> */}
-  //     {/* <Login/> */}
-  //     {/* <Error404/> */}
-  //     {/* <Cart />
-  //   </CartProvider> */}
-      <ProfilePage/>
-
-  )
+    <>
+      <Header />
+      <Welcome />
+      <Welcome />
+      <Welcome />
+         
+    </>
+  );
 }
 
-export default App
+export default App;
